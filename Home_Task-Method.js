4b9c43268e814c0d95eece1b25254f7de42b68cb
Na2Z <20102169@ittelkom-pwt.ac.id>
2023-03-03 15:00:40 +0700

    home task

---
+++ Home_Task-Method.js
@@ -0,0 +1,240 @@
+console.log("Home Task");
+// 1. Buatlah minimal 20 PROCEDURE untuk menyimpan nilai untuk 20 variable. Contoh:
+// let nama;
+
+// function setNama(argNama) {
+//     nama = argNama
+// }
+
+// 2. Buatlah 20 FUNCTION untuk mengambil nilai dari variable pada soal nomor 1, dan mengembalikan isi dari variablenya. Contoh
+// function getNama() {
+//     return nama
+// }
+// console.log(getNama())
+
+// 3. simpan di github masing masing dengan format:
+// productzilla_D6_method_take_home_task
+
+console.log("Jawaban dijadikan langsung");
+console.log("");
+// Nama     : Shahnaz Ferdiansyah
+// Class    : BE (Back End)
+
+let nama;
+let umur;
+let alamat;
+let tgllahir;
+let tmptlahir;
+let kewarganegaraan;
+let agama;
+let TB;
+let BB;
+let pekerjaan;
+let pendidikan;
+let hobi;
+let email;
+let telfon;
+let gender;
+let statuss;
+let namaBank;
+let nomorRekening;
+let pengeluaran;
+let gaji;
+
+function setNama(argNama) {
+  nama = argNama;
+}
+function getNama() {
+  return "Nama          : " + nama;
+}
+setNama("Sambalado");
+console.log(getNama());
+
+function setUmur(argumur) {
+  umur = argumur;
+}
+function getUmur() {
+  return "Umur          : " + umur;
+}
+
+setUmur(42);
+console.log(getUmur());
+
+function setAlamat(argalamat) {
+  alamat = argalamat;
+}
+function getAlamat() {
+  return "Alamat        : " + alamat;
+}
+
+setAlamat("Banyuwangi");
+console.log(getAlamat());
+
+function setTglLahir(argtgllahir) {
+  tgllahir = argtgllahir;
+}
+function getTglLahir() {
+  return "Tanggal Lahir : " + tgllahir;
+}
+
+setTglLahir("10 Maret 1981");
+console.log(getTglLahir());
+
+function setTmptLahir(arglahir) {
+  tmptlahir = arglahir;
+}
+function getTmptLahir() {
+  return "Tempat Lahir  : " + tmptlahir;
+}
+
+setTmptLahir("Malang");
+console.log(getTmptLahir());
+
+function setKewarganegaraan(argkewarganegaraan) {
+  kewarganegaraan = argkewarganegaraan;
+}
+function getKewarganegaraan() {
+  return "Status WN     : " + kewarganegaraan;
+}
+
+setKewarganegaraan("Indonesia");
+console.log(getKewarganegaraan());
+
+function setAgama(argagama) {
+  agama = argagama;
+}
+function getAgama() {
+  return "Agama         : " + agama;
+}
+
+setAgama("Kristen Ortodoks");
+console.log(getAgama());
+
+function setTB(atgtb) {
+  TB = atgtb;
+}
+function getTB() {
+  return "Tinggi Badan  : " + TB;
+}
+
+setTB("165 cm");
+console.log(getTB());
+
+function setBB(argBB) {
+  BB = argBB;
+}
+function getBB() {
+  return "Berat Badan   : " + BB;
+}
+
+setBB("56 KG");
+console.log(getBB());
+function setPekerjaan(argpekerjaan) {
+  pekerjaan = argpekerjaan;
+}
+function getPekerjaan() {
+  return "Pekerjaan     : " + pekerjaan;
+}
+
+setPekerjaan("Perangkat Desa");
+console.log(getPekerjaan());
+
+function setPendidikan(argpendidikan) {
+  pendidikan = argpendidikan;
+}
+function getPendidikan() {
+  return "Pendidikan    : " + pendidikan;
+}
+
+setPendidikan("S1 Kedinasan");
+console.log(getPendidikan());
+
+function setBank(argbank) {
+  namaBank = argbank;
+}
+function getBank() {
+  return "Bank          : " + namaBank;
+}
+
+setBank("BRI");
+console.log(getBank());
+
+function setRekening(argrekening) {
+  nomorRekening = argrekening;
+}
+function getRekening() {
+  return "Rekening      : " + nomorRekening;
+}
+
+setRekening("3156-0078329-64-8");
+console.log(getRekening());
+
+function setGaji(arggaji) {
+  gaji = arggaji;
+}
+function getGaji() {
+  return "Gaji          : " + gaji;
+}
+
+setGaji("$4000 / year");
+console.log(getGaji());
+
+function setPengeluaran(argpengeluaran) {
+  pengeluaran = argpengeluaran;
+}
+function getPengeluaran() {
+  return "Pengeluaran   : " + pengeluaran;
+}
+
+setPengeluaran("$2800 / year");
+console.log(getPengeluaran());
+
+function setHobi(arghobi) {
+  hobi = arghobi;
+}
+function getHobi() {
+  return "Hobi          : " + hobi;
+}
+
+setHobi("Memancing");
+console.log(getHobi());
+
+function setEmail(argemail) {
+  email = argemail;
+}
+function getEmail() {
+  return "Email         : " + email;
+}
+
+setEmail("Sambalado@qwerty.co.uk");
+console.log(getEmail());
+
+function setTelfon(argtelfon) {
+  telfon = argtelfon;
+}
+function getTelfon() {
+  return "No Telfon     : " + telfon;
+}
+
+setTelfon("+1-892-7831-323");
+console.log(getTelfon());
+
+function setGender(arggender) {
+  gender = arggender;
+}
+function getGender() {
+  return "Gender        : " + gender;
+}
+
+setGender("Non-Biner :v");
+console.log(getGender());
+
+function setStatus(argstatus) {
+  statuss = argstatus;
+}
+function getStatus() {
+  return "Status        : " + statuss;
+}
+
+setStatus("Married");
+console.log(getStatus());
